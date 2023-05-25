749b3aae516b74ebb3efef491a4cb42f8afabef4
Artyom Filimonov <artyom.filimonov94@gmail.com>
2023-05-25 21:47:01 +0300

    25.05

--- regular_expressions.py
+++ regular_expressions.py
@@ -3,19 +3,16 @@ import re
 
 
 def read_file(file_name):
-    with open(file_name) as f:
+    with open('phonebook_raw.csv') as f:
         rows = csv.reader(f, delimiter=",")
         contacts_list = list(rows)
     return contacts_list
 
 
-'''1. Корректировка ФИО'''
-
-
 def correct_name(contacts_list):
+    new_list = []
     pattern1 = r'^([А-ЯЁа-яё]+)(\s*)(\,?)([А-ЯЁа-яё]+)(\s*)(\,?)([А-ЯЁа-яё]*)(\,?)(\,?)(\,?)'
     pattern2 = r'\1\3\10\4\6\9\7\8'
-    new_list = list()
     for name in contacts_list:
         string_name = ','.join(name)
         sub_name = re.sub(pattern1, pattern2, string_name)
@@ -24,14 +21,10 @@ def correct_name(contacts_list):
     return new_list
 
 
-'''2. Корректируем номер телефона'''
-
-
 def correct_number(contacts_list):
-    pattern3 = r'(\+7|8)(\s*)(\(*)(\d{3})(\)*)(\s*)(\-*)(\d{3})(\s*)(\-*)(\d{2})(\s*)(\-*)(\d{2})'\
-            r'(\s*)(\(*)(доб)*(\.*)(\s*)(\d+)*(\)*)'
+    new_list = []
+    pattern3 = r'(\+7|8)(\s*)(\(*)(\d{3})(\)*)(\s*)(\-*)(\d{3})(\s*)(\-*)(\d{2})(\s*)(\-*)(\d{2})(\s*)(\(*)(доб)*(\.*)(\s*)(\d+)*(\)*)'
     pattern4 = r'+7(\4)\8-\11-\14\15\17\18\19\20'
-    new_list = list()
     for number in contacts_list:
         string_number = ','.join(number)
         sub_number = re.sub(pattern3, pattern4, string_number)
@@ -40,43 +33,31 @@ def correct_number(contacts_list):
     return new_list
 
 
-'''Объединение дублей'''
-
-
 def duplicates(contacts_list):
-    for item in contacts_list:
-        first_name = item[0]
-        last_name = item[1]
-        for element in contacts_list:
-            new_first_name = element[0]
-            new_last_name = element[1]
-            if first_name == new_first_name and last_name == new_last_name and item is not element:
-                if item[2] == '':
-                    item[2] = element[2]
-                if item[3] == '':
-                    item[3] = element[3]
-                if item[4] == '':
-                    item[4] = element[4]
-                if item[5] == '':
-                    item[5] = element[5]
-                if item[6] == '':
-                    item[6] = element[6]
-        new_list = list()
-        for artifact in contacts_list:
-            if artifact not in new_list:
-                new_list.append(artifact)
-        return new_list
+    new_list = []
+    for contact in contacts_list:
+        fio_list = ' '.join(contact[0:3]).split()
+        if len(fio_list) != 3:
+            fio_list.append('')
+        full_list = fio_list + contact[3:]
+        for contacts_new in new_list:
+            if contacts_new[:2] == full_list[:2]:
+                full_list = [x if x != '' else y for x, y in zip(contacts_new, full_list)]
+                new_list.remove(contacts_new)
+        new_list.append(full_list)
+    return new_list
 
 
 def write_file(contacts_list):
-    with open("phonebook.csv", "w") as f:
+    with open('phonebook.csv', 'w') as f:
         datawriter = csv.writer(f, delimiter=',')
         datawriter.writerows(contacts_list)
 
 
 if __name__ == '__main__':
-    result = read_file('phonebook_raw.csv')
-    result = correct_name(result)
-    result = correct_number(result)
-    result = duplicates(result)
-    write_file(result)
+  result = read_file('phonebook_raw.csv')
+  result = correct_name(result)
+  result = correct_number(result)
+  result = duplicates(result)
+  write_file(result)
+
